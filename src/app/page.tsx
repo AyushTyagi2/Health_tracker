b0016a151d6e6b206a3f9de8be74875877fcb375
Ayush_developer <2023csb1108@iitrpr.ac.in>
2025-07-21 00:58:43 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import InputForm from "./components/inputform/page";
 export default function InputPage() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-2xl font-bold mb-6 text-center">Health Tracker Input</h1>
       <InputForm />
     </div>
   );
